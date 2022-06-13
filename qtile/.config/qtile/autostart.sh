29845df7727ea377964905710e646e8480c376b7
hmanhng <hmanhng@icloud.com>
2022-06-14 00:28:38 +0700

    up

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -9,9 +9,9 @@ blueman-applet &
 # deadd-notification-center &
 picom &
 /opt/clash-for-windows-bin/cfw &
-"/opt/xdman/jre/bin/java" -Xmx1024m -jar "/opt/xdman/xdman.jar" -m &
 xfce4-clipman &
 # mate-power-manager &
 udiskie &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 xfce4-power-manager &
+cfw &
