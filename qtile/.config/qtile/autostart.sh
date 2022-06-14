bb5b6eeb5b7d8150c9f2a471a5906d7afc130e7d
hmanhng <hmanhng@icloud.com>
2022-06-15 03:33:16 +0700

    up

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -4,14 +4,12 @@ redshift &
 caffeine &
 xcape -e 'Super_L=Alt_L|F1'
 nitrogen --restore &
-nowater -T
+# nowater -T
 nm-applet &
 blueman-applet &
-# deadd-notification-center &
 picom &
-/opt/clash-for-windows-bin/cfw &
+"/opt/xdman/jre/bin/java" -Xmx1024m -jar "/opt/xdman/xdman.jar" -m &
 xfce4-clipman &
-# mate-power-manager &
 udiskie &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 xfce4-power-manager &
